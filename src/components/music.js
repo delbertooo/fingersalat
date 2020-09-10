8c97f05355180a55a8c0ef5b04f006130a757a92
Robert Worgul <robert.worgul@scitotec.de>
2020-09-11 01:22:00 +0200

    Prefer thirds from scale

--- src/components/music.js
+++ src/components/music.js
@@ -16,10 +16,21 @@ export function randomScaleNote(scale, min, max) {
     return n
 }
 
+function triple(scale, note) {
+    const ns = Scale.get(scale).notes
+    const major = transpose(note, '3M')
+    const minor = transpose(note, '3m')
+    const scaleContains = x => ns.indexOf(x) !== -1
+
+    if (scaleContains(major) || scaleContains(Note.enharmonic(major))) { return major; }
+    else if (scaleContains(minor) || scaleContains(Note.enharmonic(minor))) { return minor; }
+    else { return major; }
+}
+
 export function createFoo() {
     return Collection.range(1, 10)
         .map(() => randomScaleNote('F major', 'C4', 'G5').name)
-        .map(n => [n, transpose(n, '3M')]) // TODO: take minor if it's part of the scale
+        .map(n => [n, triple('F major', n)]) // TODO: take minor if it's part of the scale
         .map(([a, b]) => [AbcNotation.scientificToAbcNotation(a), AbcNotation.scientificToAbcNotation(b)]) // TODO: if note or enharmonic is in scale -> use scale value without alteration
 }
 
